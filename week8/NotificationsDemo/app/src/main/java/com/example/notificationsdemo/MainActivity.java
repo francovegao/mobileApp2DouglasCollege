d2faebf5d930274d7fc5115cbb6d1080b54f2247
Franco Vega <francovegao@student.douglascollege.ca>
2023-11-04 10:31:17 -0700

    week 8, notifications added

---
+++ week8/NotificationsDemo/app/src/main/java/com/example/notificationsdemo/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.notificationsdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String CHANNEL_ID = "notification101";
+
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        createNotificationChannel();
+
+    }
+
+    @SuppressLint("MissingPermission")
+    public void sendNotification(View view){
+
+        // Create an explicit intent for an Activity in your app.
+        Intent intent = new Intent(this, NotificationDetails.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.trophy_foreground)
+                .setContentTitle("Message from Oliver")
+                .setContentText("How are you doing?")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that fires when the user taps the notification.
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define.
+        int notificationId = 1;
+        notificationManager.notify(notificationId, builder.build());
+
+        Toast.makeText(MainActivity.this, "Notification sent!", Toast.LENGTH_LONG).show();
+
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is not in the Support Library.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "myChannel";
+            String description = "test notifications channel";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this.
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+}
---
+++ week8/NotificationsDemo/app/src/main/java/com/example/notificationsdemo/NotificationDetails.java
@@ -0,0 +1,14 @@
+package com.example.notificationsdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotificationDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_details);
+    }
+}
